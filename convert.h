2b6b28f7e2b9e5efbea81c2e16da2253ae7581a3
SSNTails <github@spaddlewit.com>
2024-06-12 10:49:19 -0400

    PC->Jag Sprite conversion, without compression

--- convert.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "common.h"
-
-void ConvertSpriteDataFromPCToJag(byte *lumpData, int lumpSize, byte *jagHeader, int *jagHeaderLen, byte *jagData, int *jagDataLen);
-byte *ConvertSpriteDataFromJagToPC(byte *jagHeader, int jagHeaderLen, byte *jagData, int jagDataLen, int *outputLen);
